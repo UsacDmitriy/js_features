e75c806c82ecb086fc8c790aaa575a156d17715a
Dmitry <usachev2012@gmail.com>
2019-07-30 17:20:21 +0400

    Objects

---
+++ JS_all_is_oject/scripts/script.js
@@ -0,0 +1,48 @@
+window.onload = function () { 
+    class Animal{
+        constructor(){
+            this.age = 10;
+        }
+    
+        eat(){
+        }
+    }
+    class Cat extends Animal{
+
+        constructor(){
+            super();
+            this.mustache = true;
+        }
+    
+        getVoice(){
+            return 'myaumayu'    
+        }
+    }
+    class Dog extends Animal{
+        constructor(){
+            super();
+            this.tail = true;
+        }
+    
+        getVoice(){
+            return 'gafgaf'    
+        }
+    }
+    class Tiger extends Cat {
+        constructor(){
+            super();
+            this.color = 'ginger';
+        }
+    }
+
+  
+
+    let cat = new Cat();
+    let dog = new Dog();
+    let tiger = new Tiger();
+
+    console.log(cat);
+    console.log(dog);
+    console.log(tiger);
+
+ }
